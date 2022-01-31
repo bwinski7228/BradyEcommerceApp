88a0920cce8ca97cf1f24b0df9bac119a247050f
Brady Winski <226466@NTTDATA.COM>
2022-01-31 15:21:49 -0500

    Pushing code to github repo

---
+++ BradyPortfolioApp/app/src/main/java/com/example/bradyportfolioapp/Project.java
@@ -0,0 +1,21 @@
+package com.example.bradyportfolioapp;
+
+public class Project {
+
+    String name;
+    String description;
+    int image;
+
+//    Project(String name, String description, int image) {
+//        this.name = name;
+//        this.description = description;
+//        this.image = image;
+//    }
+
+
+    public Project(String name, String description, int image) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+}
---
+++ BradyPortfolioApp/app/src/main/java/com/example/bradyportfolioapp/ProjectAdapter.java
@@ -0,0 +1,13 @@
+package com.example.bradyportfolioapp;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ProjectAdapter extends RecyclerView.Adapter<> {
+
+
+    static class ProjectViewHolder extends RecyclerView.ViewHolder {
+
+    }
+
+
+}
