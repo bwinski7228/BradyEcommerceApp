e44350caa6b8ce28902c520bf6de13abffd5a751
Brady Winski <226466@NTTDATA.COM>
2022-01-31 11:09:50 -0500

    initial commit

---
+++ PromoApp/app/src/main/java/com/example/promoapp/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.promoapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.*
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_preview.setOnClickListener {
+            onPreviewClicked()
+
+        }
+        val spinnerValues: Array<String> = arrayOf("And Dev", "And Eng", "And Butt")
+        val spinnerAdapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item, spinnerValues)
+        spinner_job_title.adapter = spinnerAdapter
+    }
+
+    private fun onPreviewClicked() {
+        val contactName = edit_text_contact_name.text.toString()
+        val contactNumber = edit_text_contact_number.text.toString()
+        val myDisplayName = edit_text_my_display_name.text.toString()
+        val includeJunior = check_box_junior.isChecked
+        val jobTitle = spinner_job_title.selectedItem?.toString()
+        val immediateStart = check_box_immediate_start.isChecked
+        val startDate = edit_text_start_date.text.toString()
+
+        //val testString = "Contact Name: $contactName, Contact Number: $contactNumber, My Display Name: $myDisplayName, Include Junior: $includeJunior, Job Title: $jobTitle, Immediate Start: $immediateStart, Start Date: $startDate"
+
+        //Toast.makeText(this, testString, Toast.LENGTH_LONG).show()
+
+        val previewActivityIntent = Intent(this, PreviewActivity::class.java)
+        previewActivityIntent.putExtra("Contact Name", contactName)
+        previewActivityIntent.putExtra("Contact Number", contactNumber)
+        previewActivityIntent.putExtra("My Display Name", myDisplayName)
+        previewActivityIntent.putExtra("Include Junior", includeJunior)
+        previewActivityIntent.putExtra("Job Title", jobTitle)
+        previewActivityIntent.putExtra("Immediate Start", immediateStart)
+        previewActivityIntent.putExtra("Start Date", startDate)
+        startActivity(previewActivityIntent)
+    }
+}
---
+++ PromoApp/functions.kts
@@ -0,0 +1,15 @@
+var myKotlinVariable: String = "My new Kotlin Variable"
+myKotlinVariable
+myKotlinVariable = "My updated variable"
+
+val myFixedValue: Int = 6
+myFixedValue
+
+var myInferredVariableType = true
+myInferredVariableType
+
+println("Hello")
+
+
+
+
