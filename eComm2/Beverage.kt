0ae8225c93bff060351efeff8b8d14664bc06133
bwinski7228 <98762122+bwinski7228@users.noreply.github.com>
2022-02-02 15:04:22 -0500

    Add files via upload

---
+++ eComm2/Beverage.kt
@@ -0,0 +1,5 @@
+package com.example.ecommerceapp
+
+import android.graphics.drawable.Drawable
+
+data class Beverage(val brand:String, val name:String, val producer:String, val price:Double, val image:Int)
---
+++ eComm2/MyRecyclerViewAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.ecommerceapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ExpandableListView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class MyRecyclerViewAdapter(private val BeveragesList:List<Beverage>, private val clickListener:(Beverage)->Unit): RecyclerView.Adapter<MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val listItem = layoutInflater.inflate(R.layout.list_item, parent, false)
+        return MyViewHolder(listItem)
+    }
+
+    override fun getItemCount(): Int {
+        return 7
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(BeveragesList[position], clickListener)
+    }
+
+}
+
+class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+    fun bind(Beverage: Beverage, clickListener:(Beverage)->Unit) {
+        view.name_text_view.text = Beverage.name
+        view.text_view_price.text = Beverage.price.toString()
+        view.image_view_project_icon.setImageResource(Beverage.image)
+        view.setOnClickListener{
+            clickListener(Beverage)
+        }
+    }
+}
