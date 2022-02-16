e18fc3fa2a4a341bfab207383342f3450b71e88a
bwinski7228 <98762122+bwinski7228@users.noreply.github.com>
2022-02-15 23:36:48 -0500

    Delete eComm2 directory

--- eComm2/Beverage.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ecommerceapp
-
-import android.graphics.drawable.Drawable
-
-data class Beverage(val brand:String, val name:String, val producer:String, val price:Double, val image:Int)
--- eComm2/SecondFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ecommerceapp
-
-
-import androidx.fragment.app.Fragment
-
-class SecondFragment:Fragment(R.layout.fragment_second) {
-}
